a0e0ce5ca4c50185e2ada50b7529bff55fd3a05c
鈴木源 <gsuzuki983@gmail.com>
2022-08-22 23:36:37 +0900

    add fetch function by single file

--- circuitBreaker/index.js
+++ circuitBreaker/index.js
@@ -1,4 +1,13 @@
-const getHttp = require('./fetch');
+const fetch = require('node-fetch');
+
+async function getHttp(context) {
+
+    context.log('calling HTTP service')
+
+    return fetch('https://red-flower-0e519b900.1.azurestaticapps.net/api/todos')
+
+        .then(res => res.json());
+}
 
 // excerpt from index.js
 module.exports = async function (context, req) {
